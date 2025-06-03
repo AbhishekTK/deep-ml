4867e23160804b9c6d739d86fd0097506c809cf2
AbhishekTK <17388553+AbhishekTK@users.noreply.github.com>
2025-06-03 06:20:14 +0000

    Solve 2 problems on 2025-06-03

---
+++ problems/0007-matrix-transformation/solution.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def transform_matrix(A: list[list[int|float]], T: list[list[int|float]], S: list[list[int|float]]) -> list[list[int|float]]:
+    if (np.linalg.det(T)) == 0 or (np.linalg.det(S)) == 0:
+        return -1
+    T_inv = np.linalg.inv(T)
+
+    transformed_matrix = np.dot(np.dot(T_inv, A), S)
+
+	return transformed_matrix
---
+++ problems/0008-calculate-2x2-matrix-inverse/solution.py
@@ -0,0 +1,8 @@
+def inverse_2x2(matrix: list[list[float]]) -> list[list[float]]:
+    a,b,c,d = matrix[0][0], matrix[0][1], matrix[1][0], matrix[1][1]
+    if (a*d - b*c) == 0:
+        return None
+    det = a*d - b*c    
+    inverse = [[d*(1/det),-b*(1/det)],[-c*(1/det),a*(1/det)]]
+
+	return inverse
